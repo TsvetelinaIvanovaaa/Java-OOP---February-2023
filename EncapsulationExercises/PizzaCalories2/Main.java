bda70e46dfdb5c3ffda6701b2dc5f3cfcd7ea99d
Tsvetelina Ivanova <Tsvetelina.Ivanovaaa@gmail.com>
2023-03-05 08:22:57 +0200

    Problems solutions for Encapsulation - Exercises
    Add tasks for Encapsulation - Exercises

---
+++ EncapsulationExercises/PizzaCalories2/Main.java
@@ -0,0 +1,10 @@
+package EncapsulationExercises.PizzaCalories2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+    }
+}
