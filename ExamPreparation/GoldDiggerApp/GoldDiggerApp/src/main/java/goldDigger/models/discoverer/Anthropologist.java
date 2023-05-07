23c52c0867cb381a43f45894b18955b4eabeb1b8
Tsvetelina Ivanova <Tsvetelina.Ivanovaaa@gmail.com>
2023-05-07 10:35:49 +0300

    Delete task from exam preparation
    Delete task

--- ExamPreparation/GoldDiggerApp/GoldDiggerApp/src/main/java/goldDigger/models/discoverer/Anthropologist.java
+++
@@ -1,8 +0,0 @@
-package goldDigger.models.discoverer;
-
-public class Anthropologist extends BaseDiscoverer{
-    private static final double ENERGY=40;
-    public Anthropologist(String name) {
-        super(name, ENERGY);
-    }
-}
--- ExamPreparation/GoldDiggerApp/GoldDiggerApp/src/main/java/goldDigger/models/discoverer/Archaeologist.java
+++
@@ -1,8 +0,0 @@
-package goldDigger.models.discoverer;
-
-public class Archaeologist extends BaseDiscoverer{
-    private static final double ENERGY=60;
-    public Archaeologist(String name) {
-        super(name, ENERGY);
-    }
-}
